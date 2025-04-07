16b46478e095a6ca1082dbb95c7acc271adadee8
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2025-04-07 20:43:20 +0300

    Add track item, list tracks, scroll tracks

---
+++ frontend/src/lib/TrackItem.svelte
@@ -0,0 +1,8 @@
+<script>
+    let {index, track} = $props();
+</script>
+
+<div style="display: flex;margin-left: -10px;align-items: center">
+    <p style="margin-right: 20px;color: #777777;white-space: break-spaces">{`${index + 1}`.padStart(5, ' ')}</p>
+    <a style="font-size: 16px">{track.title}</a>
+</div>
