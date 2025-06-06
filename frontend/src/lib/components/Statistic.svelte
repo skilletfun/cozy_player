0c9b374327edbdc0c58e34c87dc1832edf0ad9f9
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2025-06-06 22:53:56 +0300

    Move home page into separate component

---
+++ frontend/src/lib/components/Statistic.svelte
@@ -0,0 +1,21 @@
+<script>
+    import StatisticCard from "$lib/components/StatisticCard.svelte";
+    import {secondsToHumanString} from "$lib/utils.js";
+    let { data } = $props();
+</script>
+
+<div class="container">
+    <StatisticCard title="Artists" text={data.artists_total}/>
+    <StatisticCard title="Tracks" text={data.tracks_total}/>
+    <StatisticCard title="Total duration" text={secondsToHumanString(data.tracks_duration, true)}/>
+    <StatisticCard title="Total played" text={secondsToHumanString(data.tracks_played, true)}/>
+</div>
+
+<style>
+    .container {
+        margin-top: 100px;
+        display: grid;
+        justify-self: center;
+        grid-template-columns: 1fr 1fr;
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,21 +1,6 @@
 <script>
-    import StatisticCard from "$lib/components/StatisticCard.svelte";
-    import {secondsToHumanString} from "$lib/utils.js";
+    import Statistic from "$lib/components/Statistic.svelte";
     let { data } = $props();
 </script>
 
-<div class="container">
-    <StatisticCard title="Artists" text={data.artists_total}/>
-    <StatisticCard title="Tracks" text={data.tracks_total}/>
-    <StatisticCard title="Total duration" text={secondsToHumanString(data.tracks_duration, true)}/>
-    <StatisticCard title="Total played" text={secondsToHumanString(data.tracks_played, true)}/>
-</div>
-
-<style>
-    .container {
-        margin-top: 100px;
-        display: grid;
-        justify-self: center;
-        grid-template-columns: 1fr 1fr;
-    }
-</style>
+<Statistic data={ data }/>
