d88045e038d3a5a2a148a94d7473c3dcbaddf64c
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2025-08-10 17:20:23 +0300

    refactor: prevent spam db by queries

--- backend/internal/service/library.go
+++ backend/internal/service/library.go
@@ -8,8 +8,10 @@ import (
 	"gcozy_player/pkg/track"
 	"io/fs"
 	"log"
+	"maps"
 	"os"
 	"path/filepath"
+	"slices"
 	"sync"
 
 	"gorm.io/gorm"
@@ -42,14 +44,14 @@ func NewLibraryService(c container.Container) LibraryService {
 // - delete artists without tracks;
 // - delete tracks that no found while rescan. 
 func (l *libraryService) Rescan() error {
-	artistNames := map[string]uint{}
+	artistsByNames := map[string]model.Artist{}
 	artistTracks := map[uint]map[string]*model.Track{}
 
 	if artists, err := l.artistService.GetAll(); err != nil {
 		return err
 	} else {
 		for _, artist := range *artists {
-			artistNames[artist.Name] = artist.ID
+			artistsByNames[artist.Name] = artist
 		}
 	}
 
@@ -63,51 +65,96 @@ func (l *libraryService) Rescan() error {
 			artistTracks[track.ArtistID][track.Path] = &track
 		}
 	}
+	
+	if artists, err := l.ActualizeArtists(artistsByNames); err != nil {
+		return err
+	} else {
+		var wg sync.WaitGroup
+		
+		tracksForCreate := []model.Track{}
+		tracksForDelete := []model.Track{}
+		
+		for _, artist := range artists {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
 
-	var wg sync.WaitGroup
-	var err error
-	entries, _ := os.ReadDir(l.conf.MusicFolder)
-	log.Println("Scan music directory: ", l.conf.MusicFolder)
-	for _, entry := range entries {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			if !entry.IsDir() {
-				return
-			}
-
-			// Create artist if not exist, add to maps
-			if _, ok := artistNames[entry.Name()]; !ok {
-				artist := &model.Artist{Name: entry.Name()}
-				if err = l.artistService.Create(artist); err != nil {
-					return
+				if _, ok := artistTracks[artist.ID]; !ok {
+					artistTracks[artist.ID] = map[string]*model.Track{}
 				}
-				if err = l.artistService.UpdateCover(artist); err != nil {
-					return
+				
+				newTracks := l.CollectNewTracks(artist, artistTracks[artist.ID])
+				tracksForCreate = append(tracksForCreate, *newTracks...)
+
+				for _, v := range artistTracks[artist.ID] {
+					tracksForDelete = append(tracksForDelete, *v)
 				}
-				artistNames[artist.Name] = artist.ID
-				artistTracks[artist.ID] = map[string]*model.Track{}
+			}()
+		}
+		wg.Wait()
+		
+		// Create new tracks
+		if len(tracksForCreate) > 0 {
+			if err := l.trackService.BulkCreate(&tracksForCreate); err != nil {
+				return err
 			}
-
-			artistId := artistNames[entry.Name()]
-			artistPath := l.conf.MusicFolder + "/" + entry.Name()
-			newTrackPaths := l.CollectNewTracks(artistPath, artistTracks[artistId])
-
-			// Delete from DB tracks because not found while WalkDir
-			for _, v := range artistTracks[artistId] {
-				if err = l.trackService.Delete(v); err != nil {
-					return
-				}
+		}
+		
+		// Delete tracks that not found
+		if len(tracksForDelete) > 0 {
+			if err := l.trackService.BulkDelete(&tracksForDelete); err != nil {
+				return err
 			}
-
-			if err = l.CreateNewTracks(newTrackPaths, artistId); err != nil {
-				return
-			}
-		}()
+		}
 	}
-	wg.Wait()
-
+	
 	return l.artistService.DeleteWithoutTracks()
+}
+
+func (l *libraryService) ActualizeArtists(artistsByNames map[string]model.Artist) (map[string]model.Artist, error) {
+	entries, _ := os.ReadDir(l.conf.MusicFolder)
+	newArtists := []model.Artist{}
+	existedArtists := map[string]model.Artist{}
+	
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+
+		if _, ok := artistsByNames[entry.Name()]; !ok {
+			log.Println("Found new artist: ", entry.Name())
+			artist := model.Artist{Name: entry.Name()}
+			newArtists = append(newArtists, artist)
+		} else {
+			existedArtists[entry.Name()] = artistsByNames[entry.Name()]
+			delete(artistsByNames, entry.Name())
+		}
+	}
+	
+	if len(newArtists) > 0 {
+		log.Println("Found new artist for create: ", len(newArtists))
+		if err := l.artistService.BulkCreate(&newArtists); err != nil {
+			return nil, err
+		}
+	}
+	 
+	for _, artist := range newArtists {
+		existedArtists[artist.Name] = artist
+	}
+	
+	artistsForDelete := slices.Collect(maps.Values(artistsByNames))
+	if len(artistsForDelete) > 0 {
+		log.Println("Found artist for delete: ", len(artistsForDelete))
+		if err := l.artistService.BulkDelete(&artistsForDelete); err != nil {
+			return nil, err
+		}
+	}
+	
+	if err := l.artistService.UpdateCovers(); err != nil {
+		return nil, err
+	}
+	
+	return existedArtists, nil
 }
 
 // CollectNewTracks returns paths to new tracks.
@@ -116,8 +163,10 @@ func (l *libraryService) Rescan() error {
 // 1) check file is track file, skip if doesn't;
 // 2) if track file in library, remove it from tracks to prevent being deleted;
 // 3) if track file not in library, add it to return result.
-func (l *libraryService) CollectNewTracks(walkPath string, tracks map[string]*model.Track) []string {
+func (l *libraryService) CollectNewTracks(artist model.Artist, tracks map[string]*model.Track) *[]model.Track {
+	walkPath := l.conf.MusicFolder + "/" + artist.Name
 	log.Println("Scan artist directory: ", walkPath)
+	
 	var newTrackPaths []string
 	filepath.WalkDir(
 		walkPath,
@@ -133,17 +182,13 @@ func (l *libraryService) CollectNewTracks(walkPath string, tracks map[string]*mo
 		},
 	)
 	log.Println("Found ", len(newTrackPaths), "tracks in artist: ", walkPath)
-	return newTrackPaths
-}
-
-// CreateNewTracks is the method to create new tracks for provided artist 
-func (l *libraryService) CreateNewTracks(trackPaths []string, artistId uint) error {
+	
 	var newTracks []model.Track
 
 	m := sync.Mutex{}
 	wg := sync.WaitGroup{}
 
-	for _, trackPath := range trackPaths {
+	for _, trackPath := range newTrackPaths {
 		wg.Add(1)
 		go func(path string) {
 			defer wg.Done()
@@ -157,7 +202,7 @@ func (l *libraryService) CreateNewTracks(trackPaths []string, artistId uint) err
 				Album:    info.Tags.Album,
 				Duration: uint16(info.Duration),
 				Path:     trackPath,
-				ArtistID: artistId,
+				ArtistID: artist.ID,
 			}
 
 			m.Lock()
@@ -166,5 +211,6 @@ func (l *libraryService) CreateNewTracks(trackPaths []string, artistId uint) err
 		}(trackPath)
 	}
 	wg.Wait()
-	return l.trackService.BulkCreate(&newTracks)
+	
+	return &newTracks
 }
