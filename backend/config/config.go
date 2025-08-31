90b669be1bbc2c1798d84b16a820d571280f0249
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2025-08-31 22:50:09 +0300

    feat: use env vars for config vars

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type Config struct {
 	MusicFolder string
 	Database    string
@@ -9,6 +11,14 @@ func LoadConfig() *Config {
 	config := Config{
 		MusicFolder: "/music",
 		Database:    "/db/database.db",
+	}
+
+	if music_folder, exists := os.LookupEnv("MUSIC_FOLDER"); exists {
+		config.MusicFolder = music_folder
+	} 
+
+	if database, exists := os.LookupEnv("DATABASE"); exists {
+		config.Database = database
 	}
 
 	return &config
