17df627d00c545d466a35d3c2ec15265f5116875
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2026-01-24 21:31:47 +0300

    feature: add ActionButton

---
+++ frontend/src/lib/components/ActionButton.svelte
@@ -0,0 +1,27 @@
+<button onclick={() => onclick()} class="row center hover">
+  <i class="flex fal fa-{icon}"></i>
+  <p>{title}</p>
+</button>
+
+<script>
+  import { page } from "$app/state";
+  let { title, icon, onclick } = $props();
+</script>
+
+<style>
+  button {
+    height: 40px;
+    padding-right: 15px;
+    padding-left: 15px;
+    border-radius: 20px;
+    background-color: transparent;
+    font-size: 12pt;
+    color: #e08b5d;
+  }
+
+  i {
+    width: 32px;
+    height: 32px;
+    align-items: center;
+  }
+</style>
--- frontend/src/lib/components/Header.svelte
+++ frontend/src/lib/components/Header.svelte
@@ -1,8 +1,12 @@
 <div class="navigation row center mt-1">
   <NavButton title="Home" icon="home" href="/" />
   <NavButton title="Artists" icon="user-music" href="/artists" />
+  <ActionButton title="Rescan" icon="redo" onclick={Rescan} />
 </div>
 
 <script>
   import NavButton from "$lib/components/NavButton.svelte";
+  import ActionButton from "./ActionButton.svelte";
+  import { Rescan } from "$lib/services/library";
+  
 </script>
