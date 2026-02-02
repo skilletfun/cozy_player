795d799c29f2c60335f46ec9c3b7cbebcda21ed4
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2026-02-02 23:10:20 +0300

    fix: year out of range

--- backend/pkg/tags/tags.go
+++ backend/pkg/tags/tags.go
@@ -23,11 +23,16 @@ func GetTags(filename string) (Tags, error) {
 		return Tags{}, err
 	}
 
+	year := ""
+	if len(tags[taglib.Date]) > 0 {
+		year = tags[taglib.Date][0]
+	}
+
 	return Tags{
 		Title:    tags[taglib.Title][0],
 		Artist:   tags[taglib.Artist][0],
 		Album:    tags[taglib.Album][0],
-		Year:     tags[taglib.Date][0],
+		Year:     year,
 		Duration: int(properties.Length.Seconds()),
 	}, nil
 }
