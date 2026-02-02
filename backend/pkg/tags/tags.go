e5fe9b202687fe3ba2d1db091d9dbb09a438248d
skilletfun <skilletfun.laptew.sergey@yandex.ru>
2026-02-02 23:16:46 +0300

    fix: album out of range

--- backend/pkg/tags/tags.go
+++ backend/pkg/tags/tags.go
@@ -27,11 +27,16 @@ func GetTags(filename string) (Tags, error) {
 	if len(tags[taglib.Date]) > 0 {
 		year = tags[taglib.Date][0]
 	}
+	
+	album := ""
+	if len(tags[taglib.Album]) > 0 {
+		album = tags[taglib.Album][0]
+	}
 
 	return Tags{
 		Title:    tags[taglib.Title][0],
 		Artist:   tags[taglib.Artist][0],
-		Album:    tags[taglib.Album][0],
+		Album:    album,
 		Year:     year,
 		Duration: int(properties.Length.Seconds()),
 	}, nil
